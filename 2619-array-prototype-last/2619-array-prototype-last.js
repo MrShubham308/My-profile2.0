ca5f94fd1eb48ffc41c3e99fe65ab1d6f0f40f2c
Shubham Vishwakarma <itzshubham81@gmail.com>
2026-08-12 10:34:03 +0530

    Time: 51 ms (10.78%), Space: 54.1 MB (14.62%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,27 @@
+Array.prototype.last = function() {
+    if (this.length === 0) {
+        return -1;
+    }
+
+    return this[this.length - 1];
+};
+const nums = [null, {}, 3];
+
+console.log(nums.last()); // 3
+
+const arr = [];
+
+console.log(arr.last()); // -1
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    
+};
+Array.prototype.last = function() {
+    return this.length === 0 ? -1 : this[this.length - 1];
+};
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
